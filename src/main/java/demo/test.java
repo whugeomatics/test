877a62c743eb5b84553dc5090e5a9d89a0e2e172
zhangrui <zhangrui@huace.cn>
2026-05-13 11:18:39 +0800

    init

--- src/main/java/demo/test.java
+++ src/main/java/demo/test.java
@@ -1,7 +1,13 @@
-package com;
+package demo;
 
 /**
-  * @description:
-  * @date: 2026-05-13
-  */public class test {
+ * @description:
+ * @date: 2026-05-13
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int a = 1 / 0;
+        System.out.println("hello world");
+    }
 }
